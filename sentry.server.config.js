7dab81f5057d64c7ea4cb3d830b97a99b70a6bb8
Zbigniew Jędraczka <mr.zbigniew@gmail.com>
2026-06-12 00:41:09 +0200

    chore(sentry): update dsn for client and server

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/astro";
 
 Sentry.init({
-  dsn: "https://6cdf74bcb6c5e263a1d42d97e9001e36@o4511548290170880.ingest.de.sentry.io/4511548297117777",
+  dsn: "https://8012c6d112a073e92bd924e71e7979d4@o4511548896706560.ingest.de.sentry.io/4511548902146128",
   // To disable sending user data, uncomment the line below. For more info visit:
   // https://docs.sentry.io/platforms/javascript/guides/astro/configuration/options/#dataCollection
   // dataCollection: { userInfo: false },
